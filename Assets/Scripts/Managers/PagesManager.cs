6a46f915801dd110b14a71843b64ff784ba75258
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-09-30 08:15:35 +0700

    Fix Commit

---
+++ Assets/Scripts/Managers/PagesManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PagesManager : MonoBehaviour
+{
+    Scene activeScene;
+    string activeSceneName;
+
+    private void Update()
+    {
+        activeScene = SceneManager.GetActiveScene();
+        activeSceneName = activeScene.name;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if(activeSceneName == "Menu")
+            {
+                OnApplicationQuit();
+            }
+
+            if(activeSceneName == "Level_01")
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
